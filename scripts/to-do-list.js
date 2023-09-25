ebd810c90ca85fa88f4cb0834c1e82dc188f543d
cristian-enache <cristian.enache01@gmail.com>
2023-09-25 19:42:58 +0300

    feat: new

--- scripts/to-do-list.js
+++ scripts/to-do-list.js
@@ -1,14 +1,42 @@
 const todoList = [];
 
+rendorTodolist();
+
+function rendorTodolist(){
+
+    let todoListHTML = '';
+
+    for(let i = 0; i < todoList.length; i++){
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const {name, dueDate} = todoObject;
+
+        const html =`
+        <div>${name}</div>
+        <div>${dueDate}</div>
+            <button onclick ="todoList.splice(${i}, 1); rendorTodolist(); "class="btn-delete" >Delete</button>
+        `;
+        todoListHTML += html;
+    }
+    //console.log(todoListHTML);
+    document.querySelector('.js-to-do-List').innerHTML = todoListHTML;
+    }
+
 function addTdl(){
     const inputElement = document.querySelector('.js-input');
     const name = inputElement.value;
+
+    const dateInputElemnt = document.querySelector('.js-dueDate-input');
+    const dueDate = dateInputElemnt.value;
     
-    todoList.push(name);
+    todoList.push( {name, dueDate} );
+
+    //console.log(name);
 
     inputElement.value = "";
 
-    document.querySelector('.show').innerHTML = todoList;
+    //document.querySelector('.show').innerHTML = todoList;
+
+    rendorTodolist();
 }
-
-
